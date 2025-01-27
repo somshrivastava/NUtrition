28eae372a47a9323ac2c666deb26d23af5a47309
Naman Rusia <naman.rusia14@gmail.com>
2025-01-27 12:47:43 -0500

    naman changes

---
+++ backend/config.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+# cors = CROSS ORIGIN REQUESTS. Our frontend server is running on (most likely)
+# Local Host 5173, and our backend server will be set to run on Local Host 8000. 
+
+app = Flask(__name__) # making the flask app instance
+CORS(app) # wrapping the app with CORS
+
+
+
+# now here we configure the rest of our configs and databases. 
+
+db  = None
+# example setup: db = SQLAlchemy(app)
