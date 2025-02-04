8be252f16f9b6ba8467e5750046569d345c18091
Som Shrivastava <som.shrivastava@gmail.com>
2025-02-04 16:18:00 -0500

    Working on the menu section of the menu page

--- frontend/src/pages/Menu.tsx
+++ frontend/src/pages/Menu.tsx
@@ -4,10 +4,66 @@ import LeftArrowSvg from "./../assets/left-arrow.svg";
 import RightArrowSvg from "./../assets/right-arrow.svg";
 import { Dropdown } from "primereact/dropdown";
 import { Button } from "primereact/button";
+import { Foods, FoodStation } from "../schema.type";
 
 const Menu: React.FC = () => {
   const [selectedDiningHall, setSelectedDiningHall] = useState(null);
   const diningHalls = [{ name: "International Village" }, { name: "Stetson East" }];
+  const foods: Foods = {
+    breakfast: [
+      {
+        name: "English Breakfast Baked Beans",
+        description: "Traditional tomato baked beans with maple and spices",
+        foodStation: FoodStation.CUCINA,
+        nutritionalInfo: null,
+        servingSize: {
+          value: 1,
+          unit: "cup",
+        },
+        dietaryRestrictions: [
+          {
+            symbol: "AG",
+            name: "Avoiding Gluten",
+            description: "Menu items made without gluten containing ingredients",
+          },
+          {
+            symbol: "VG",
+            name: "Vegan",
+            description: "Contains no animal-based ingredients or by-products",
+          },
+        ],
+      },
+      {
+        name: "English Breakfast Baked Beans",
+        description: "Traditional tomato baked beans with maple and spices",
+        foodStation: FoodStation.CUCINA,
+        nutritionalInfo: null,
+        servingSize: {
+          value: 1,
+          unit: "cup",
+        },
+        dietaryRestrictions: [
+          {
+            symbol: "AG",
+            name: "Avoiding Gluten",
+            description: "Menu items made without gluten containing ingredients",
+          },
+          {
+            symbol: "VG",
+            name: "Vegan",
+            description: "Contains no animal-based ingredients or by-products",
+          },
+        ],
+      },
+    ],
+    lunch: [],
+    dinner: [],
+    everyday: [],
+  };
+
+  // TODO:
+  // need to write a method that will take foods and rearrange the data
+  // into an array of food stations with the foods associated with each food station
 
   return (
     <>
@@ -30,20 +86,27 @@ const Menu: React.FC = () => {
           <h3 className="page-timing-title">8:00 AM to 10:00 PM</h3>
         </div>
         <div className="page-mealtime">
-          <div className="page-mealtime-breakfast">
-            <Button label="Breakfast" severity="danger" />
-          </div>
-          <div className="page-mealtime-lunch">
-            <Button label="Lunch" />
-          </div>
-          <div className="page-mealtime-dinner">
-            <Button label="Dinner" />
-          </div>
+          <Button className="page-mealtime-breakfast" label="Breakfast" severity="danger" />
+          <Button className="page-mealtime-lunch" label="Lunch" severity="secondary" />
+          <Button className="page-mealtime-dinner" label="Dinner" severity="secondary" />
         </div>
         <div className="page-menu">
           <div className="page-menu-section">
-            <div className="page-menu-section-title">{/* insert title */}</div>
-            <div className="page-menu-section-item">{/* insert item */}</div>
+            <h1 className="page-menu-section-title">Cucina</h1>
+            <div className="page-menu-section-items">
+              {foods.breakfast.map((item) => (
+                <div className="page-menu-section-item">
+                  <div className="page-menu-section-item-content">
+                    <h3 className="page-menu-section-item-content-title">
+                      English Breakfast Baked Beans (PR) (AG)
+                    </h3>
+                    <p className="page-menu-section-item-content-description">
+                      Traditional tomato baked beans with maple and spices{" "}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
