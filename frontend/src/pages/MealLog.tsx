6637ef17b59da11c9bd33099d5f982afe7c8614b
Som Shrivastava <som.shrivastava@gmail.com>
2025-02-09 16:32:07 -0500

    MealLog UI complete

--- frontend/src/pages/MealLog.tsx
+++ frontend/src/pages/MealLog.tsx
@@ -5,6 +5,8 @@ import "./../styles/MealLog.scss";
 import MacrosChart from "../components/MacrosChart";
 import Legend from "../components/Legend";
 import Meals from "../components/Meals";
+import { InputText } from "primereact/inputtext";
+import { ProgressBar } from "primereact/progressbar";
 
 const MealLog: React.FC = () => {
   return (
@@ -13,16 +15,13 @@ const MealLog: React.FC = () => {
         <Date />
         <div className="page-calories">
           <div className="page-calories-value">
-            <div className="page-calories-value-consumed"></div>
-            <div className="page-calories-value-goal"></div>
+            <div className="page-calories-value-consumed">450 / </div>
+            <InputText className="page-calories-value-goal" keyfilter="int" placeholder="" />
+            <div className="page-calories-value-units">cals</div>
           </div>
-          <div className="page-calories-bar">
-            <div className="page-calories-bar-consumed"></div>
-          </div>
+          <ProgressBar className="page-calories-bar" value={37}></ProgressBar>
         </div>
-        <div className="page-pie-chart">
-          <MacrosChart />
-        </div>
+        <MacrosChart />
         <Legend />
         <Meals />
       </div>
